d8bb933b683b2e04ab88406507dad3fddb5804a7
Ziadmohamed345 <117119051+Ziadmohamed345@users.noreply.github.com>
2023-04-24 01:55:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -36,29 +36,62 @@ def RE2NFA(regex):
     }
 
     # First Step: Removing brackets
-    # TODO: Fix bug when there are more than one brackets
+    # TODO: Fix bug when there are more than one bracket
     # TODO: Fix bug when there is asterisk after brackets
-    startIndex = 0 # Index of first bracket
-    endIndex = 0 # Index of last bracket
-    for i in regex:
-        if i == '(': # Find first bracket
-            startIndex = regex.index(i)
-            regex = regex.replace(i, '')
-        elif i == ')': # Find last bracket
-            endIndex = regex.index(i)
-            regex = regex.replace(i, '')
+    startIndex = [] # List of indices for opening brackets
+    endIndex = [] # List of indices for closing brackets
 
-            insideBrackets = regex[startIndex:endIndex].split('|') # Split content inside brackets
-            rightSide = regex[endIndex:] # Get right side of regex after last bracket
+    for i, char in enumerate(regex):
+        if char == '(':
+            startIndex.append(i)
+        elif char == ')':
+            endIndex.append(i)
+    for i in range(len(startIndex)):
+        insideBrackets = regex[startIndex[i]+1:endIndex[i]].split('|') # Split content inside current set of brackets
+        rightSide = regex[endIndex[i]+1:] # Get right side of regex after current set of brackets
 
-            regex = regex[:startIndex] # Get left side of regex before first bracket
+        # Get left side of regex before current set of brackets
+        if i == 0: # First set of brackets
+            leftSide = regex[:startIndex[i]]
+        else: # Subsequent sets of brackets
+            leftSide = regex[endIndex[i-1]+1:startIndex[i]]
 
-            # Concatenate right side of regex to each element inside brackets
-            for j in insideBrackets:
-                regex += j + rightSide + '|'
+        # Concatenate right side of regex to each element inside current set of brackets
+        for j in insideBrackets:
+            leftSide += j + rightSide + '|'
 
-            # Remove last '|' from regex
-            regex = regex[:-1]
+        # Remove last '|' from current set of brackets
+        leftSide = leftSide[:-1]
+
+        # Reassemble the regex with the modifications
+        regex = regex[:startIndex[i]] + leftSide + regex[endIndex[i]+1:]
+
+    # Find all brackets and append their indices to the respective lists
+    # for i, char in enumerate(regex):
+    #     if char == '(':
+    #         startIndex.append(i)
+    #     elif char == ')':
+    #         endIndex.append(i)
+            
+    # for i in regex:
+    #     if i == '(': # Find first bracket
+    #         startIndex = regex.index(i)
+    #         regex = regex.replace(i, '')
+    #     elif i == ')': # Find last bracket
+    #         endIndex = regex.index(i)
+    #         regex = regex.replace(i, '')
+
+    #         insideBrackets = regex[startIndex:endIndex].split('|') # Split content inside brackets
+    #         rightSide = regex[endIndex:] # Get right side of regex after last bracket
+
+    #         regex = regex[:startIndex] # Get left side of regex before first bracket
+
+    #         # Concatenate right side of regex to each element inside brackets
+    #         for j in insideBrackets:
+    #             regex += j + rightSide + '|'
+
+    #         # Remove last '|' from regex
+    #         regex = regex[:-1]
     
     # Second Step: Splitting regex OR operator
     T = regex.split('|')
@@ -93,9 +126,8 @@ def RE2NFA(regex):
     printTransitionTable(transition_table)
 
 
-input_regex_1 = '(a|b)*'
+input_regex_1 = '((a|b))*'
 RE2NFA(input_regex_1)
 print('\n')
 input_regex_2 = '10|(0|11)0*1'
 RE2NFA(input_regex_2)
-
