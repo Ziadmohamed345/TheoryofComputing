7bd5d193af6e97d7900df2c8fb99345fab15812d
AnasMations <anas.ahassan@yahoo.com>
2023-05-25 00:08:13 +0300

    Update cfg_to_cnf.py

--- cfg_to_cnf.py
+++ cfg_to_cnf.py
@@ -14,16 +14,39 @@ def cfg_2_cnf(cfg):
     cnf = cfg
     
     # 1st Create New Start State
-    #cnf['S0'] = cnf['S']
+    print("---------------------------------------")
+    print("1st Create New Start State")
+    print("---------------------------------------")
+    cnf['S0'] = cnf['S']
+    print_cfg(cnf)
 
-    # 2nd Eliminate e productions
-    Eliminate_e_productions(cnf)
+    # 2nd Eliminate nullable productions
+    print("---------------------------------------")
+    print("2nd Eliminate e productions")
+    print("---------------------------------------")
+    Eliminate_nullable_productions(cnf)
+    print_cfg(cnf)
 
     # 3rd Eliminate unit productions
+    print("---------------------------------------")
+    print("3rd Eliminate unit productions")
+    print("---------------------------------------")
+    Eliminate_unit_productions(cnf)
+    print_cfg(cnf)
 
     # 4th Eliminate useless productions
+    print("---------------------------------------")
+    print("4th Eliminate useless productions")
+    print("---------------------------------------")
+    Eliminate_useless_productions(cnf)
+    print_cfg(cnf)
 
     # 5th Convert to Chomsky Normal Form
+    print("---------------------------------------")
+    print("5th Convert to Chomsky Normal Form")
+    print("---------------------------------------")
+    Convert_to_CNF(cnf)
+    print_cfg(cnf)
 
     return cnf
 
@@ -31,7 +54,7 @@ def cfg_2_cnf(cfg):
 # Helper Functions
 ##############################################################
 
-def Eliminate_e_productions(dict):
+def Eliminate_nullable_productions(dict):
     list_to_remove = []
     for key, value in dict.items():
 
@@ -48,17 +71,37 @@ def Eliminate_e_productions(dict):
         for key, value in dict.items():
             for e in value:
                 if e_to_remove in e and e_to_remove != e:
-                    value.append(e.replace(e_to_remove, '_'))
+                    value.append(e.replace(e_to_remove, ''))
 
 
     return dict
 
 def Eliminate_unit_productions(dict):
+    has_unit_production = True
+
+    while has_unit_production:
+        has_unit_production = False
+
+        for key, value in dict.items():
+            new_values = []
+
+            for prod_value in value:
+                if len(prod_value) == 1 and prod_value.isupper() and prod_value != key:
+                    has_unit_production = True
+                    new_values.extend(dict[prod_value])
+                else:
+                    new_values.append(prod_value)
+
+            dict[key] = new_values
 
     return dict
 
 def Eliminate_useless_productions(dict):
     
+    return dict
+
+def Convert_to_CNF(dict):
+
     return dict
 
 ##############################################################
@@ -77,11 +120,12 @@ CFG_test = {'S': ['ASB', 'a'],
        'A': ['aAS', 'a', 'e'],
        'B': ['SbS', 'A', 'bb']}
 
-print("Before")
+print("---------------------------------------")
+print("Starting CFG:")
+print("---------------------------------------")
 print_cfg(CFG_test)
-print("-------------------------")
-print("After")
-print_cfg(cfg_2_cnf(CFG_test))
+
+cfg_2_cnf(CFG_test)
 
 
 ##############################################################
